e49eb683dc2bb8729d5b11b15d24036ce5962b3a
Matt Henderson <matth@plato.lucidsw.com.au>
2020-04-29 21:39:39 +1000

    Add firebird to coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -10,7 +10,9 @@ rm -rf html
 
 lcov --directory . --zerocounters
 
-DS_MODULE_PATH=/home/matth/devel/datenstrom/coverage/src/sqlite:/home/matth/devel/datenstrom/coverage/src/mysql ctest
+DS_MODULE_PATH=/home/matth/devel/datenstrom/coverage/src/sqlite:\
+/home/matth/devel/datenstrom/coverage/src/mysql:\
+/home/matth/devel/datenstrom/coverage/src/firebird ctest
 
 lcov --directory . --capture --output-file /tmp/coverage.info
 lcov --remove /tmp/coverage.info '*/test*/*' '/usr/*' --output-file /tmp/coverage.cleaned
