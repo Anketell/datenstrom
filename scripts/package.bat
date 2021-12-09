f74fcc34d8b6fa2c4d970e42594e9573453e0906
Matt Henderson <mhenderson@prydaanz.com>
2021-12-09 13:44:04 +1100

    Minor clean up

--- scripts/package.bat
+++ scripts/package.bat
@@ -4,11 +4,7 @@ CALL %~dp0\make_image.bat
 
 pushd %~dp0\..
 
-REM cp -v win32\nuget\packages.config      build\image
-
 cp -v win32\nuget\datenstrom.nuspec    build\image
-REM cp -v win32\nuget\datenstrom.props     build\image
-
 cp -v scripts\VERSION                  build\image
 
 cd build\image
