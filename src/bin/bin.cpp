93680ff8e2f47c6ce4baccd661a062a3ed0bab78
Matt Henderson <matt.henderson@lucidsw.com.au>
2022-02-02 12:28:51 +1100

    Add binary streams and buffers

---
+++ src/bin/bin.cpp
@@ -0,0 +1,474 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 Lucid Systems Pty Ltd - All Rights Reserved
+//
+//-----------------------------------------------------------------------------
+
+#include <bin/bin.h>
+#include <dsutil/endian.h>
+
+//-----------------------------------------------------------------------------
+
+namespace ds
+{
+
+//-----------------------------------------------------------------------------
+
+namespace bin
+{
+
+//-----------------------------------------------------------------------------
+
+struct filter_t
+{
+   virtual uint16_t operator() ( uint16_t u ) = 0;
+   virtual uint32_t operator() ( uint32_t u ) = 0;
+   virtual uint64_t operator() ( uint64_t u ) = 0;
+};
+
+//-----------------------------------------------------------------------------
+
+struct le_filter_t : filter_t
+{
+   virtual uint16_t operator() ( uint16_t u ) override { return util::endian::htol( u ); }
+   virtual uint32_t operator() ( uint32_t u ) override { return util::endian::htol( u ); }
+   virtual uint64_t operator() ( uint64_t u ) override { return util::endian::htol( u ); }
+};
+
+//-----------------------------------------------------------------------------
+
+struct be_filter_t : filter_t
+{
+   virtual uint16_t operator() ( uint16_t u ) override { return util::endian::htob( u ); }
+   virtual uint32_t operator() ( uint32_t u ) override { return util::endian::htob( u ); }
+   virtual uint64_t operator() ( uint64_t u ) override { return util::endian::htob( u ); }
+};
+
+//-----------------------------------------------------------------------------
+
+static le_filter_t le_filter;
+static be_filter_t be_filter;
+static le_filter_t null_filter;
+
+//-----------------------------------------------------------------------------
+
+istream::istream( std::streambuf * sb ) :
+m_sb( sb ),
+m_filter( &null_filter )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( int8_t & i ) 
+{
+   return *this >> *reinterpret_cast< uint8_t * >( &i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( int16_t & i ) 
+{
+   return *this >> *reinterpret_cast< uint16_t * >( &i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( int32_t & i ) 
+{
+   return *this >> *reinterpret_cast< uint32_t * >( &i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( int64_t & i ) 
+{
+   return *this >> *reinterpret_cast< uint64_t * >( &i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( uint8_t & u ) 
+{
+   m_sb->sgetn( reinterpret_cast< char * >( &u ), sizeof( u ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( uint16_t & u ) 
+{
+   uint16_t tmp;
+   m_sb->sgetn( reinterpret_cast< char * >( &tmp ), sizeof( u ) );
+   u = ( *m_filter )( tmp );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( uint32_t & u ) 
+{
+   uint32_t tmp;
+   m_sb->sgetn( reinterpret_cast< char * >( &tmp ), sizeof( u ) );
+   u = ( *m_filter )( tmp );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( uint64_t & u ) 
+{
+   uint64_t tmp;
+   m_sb->sgetn( reinterpret_cast< char * >( &tmp ), sizeof( u ) );
+   u = ( *m_filter )( tmp );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::istream & istream::operator >> ( double & d ) 
+{
+   m_sb->sgetn( reinterpret_cast< char * >( &d ), sizeof( d ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+void istream::filter( filter_t * filter )
+{
+   m_filter = filter;
+}
+
+//-----------------------------------------------------------------------------
+
+void istream::rdbuf( std::streambuf * sb )
+{
+   m_sb = sb;
+}
+
+//-----------------------------------------------------------------------------
+
+std::streambuf * istream::rdbuf( void ) const
+{
+   return m_sb;
+}
+
+//-----------------------------------------------------------------------------
+
+ostream::ostream( std::streambuf * sb ) :
+m_sb( sb ),
+m_filter( &null_filter )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( int8_t i )
+{
+   return *this << static_cast< uint8_t >( i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( int16_t i ) 
+{
+   return *this << static_cast< uint16_t >( i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( int32_t i ) 
+{
+   return *this << static_cast< uint32_t >( i );
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( int64_t i ) 
+{
+   return *this << static_cast< uint64_t >( i );
+}
+
+//-----------------------------------------------------------------------------
+
+
+ds::ostream & ostream::operator << ( uint8_t u ) 
+{
+   m_sb->sputn( reinterpret_cast< char * >( &u ), sizeof( u ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( uint16_t u ) 
+{
+   auto tmp = ( *m_filter )( u );
+   m_sb->sputn( reinterpret_cast< char * >( &tmp ), sizeof( u ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( uint32_t u ) 
+{
+   auto tmp = ( *m_filter )( u );
+   m_sb->sputn( reinterpret_cast< char * >( &tmp ), sizeof( u ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+ds::ostream & ostream::operator << ( uint64_t u ) 
+{
+   auto tmp = ( *m_filter )( u );
+   m_sb->sputn( reinterpret_cast< char * >( &tmp ), sizeof( u ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+
+ds::ostream & ostream::operator << ( double d ) 
+{
+   m_sb->sputn( reinterpret_cast< char * >( &d ), sizeof( d ) );
+   return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+void ostream::filter( filter_t * filter )
+{
+   m_filter = filter;
+}
+
+//-----------------------------------------------------------------------------
+
+void ostream::rdbuf( std::streambuf * sb )
+{
+   m_sb = sb;
+}
+
+//-----------------------------------------------------------------------------
+
+std::streambuf * ostream::rdbuf( void ) const
+{
+   return m_sb;
+}
+
+//-----------------------------------------------------------------------------
+
+streamwrap::streamwrap( void * buffer, std::streamsize length )
+{
+   setbuf( reinterpret_cast< char * >( buffer ), length );
+}
+
+//-----------------------------------------------------------------------------
+
+std::streambuf * streamwrap::setbuf( char * s, std::streamsize n )
+{
+   setp( s, s + n );
+   setg( s, s, s + n );
+
+   return this;
+}
+
+//-----------------------------------------------------------------------------
+
+int streamwrap::underflow( void )
+{
+   if ( !gremaining() )
+      throw buffer_underrun();
+
+   return std::streambuf::underflow();
+}
+
+//-----------------------------------------------------------------------------
+
+int streamwrap::overflow( int ch ) 
+{
+   if ( !premaining() )
+      throw buffer_overrun();
+
+   return std::streambuf::overflow( ch );
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::xsgetn( char * s, std::streamsize count )
+{
+   if ( gremaining() < count )
+      throw buffer_underrun();
+
+   return std::streambuf::xsgetn( s, count );
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::xsputn( const char * s, std::streamsize count )
+{
+   if ( premaining() < count )
+      throw buffer_overrun();
+
+   return std::streambuf::xsputn( s, count );
+}
+
+//-----------------------------------------------------------------------------
+
+void * streamwrap::buffer( void ) const
+{
+   return pbase();
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::size( void ) const
+{
+   return epptr() - pbase();
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::plength( void ) const
+{
+   return pptr() - pbase();
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::premaining( void ) const
+{
+   return epptr() - pptr();
+}
+
+//-----------------------------------------------------------------------------
+
+void * streamwrap::ppos( void ) const
+{
+   return pptr();
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::glength( void ) const
+{
+   return gptr() - eback();
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize streamwrap::gremaining( void ) const
+{
+   return egptr() - gptr();
+}
+
+//-----------------------------------------------------------------------------
+
+void * streamwrap::gpos( void ) const
+{
+   return gptr();
+}
+
+//-----------------------------------------------------------------------------
+
+streambuf::streambuf( std::streamsize length ) :
+streamwrap( new char[ length ], length )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+streambuf::~streambuf( void )
+{
+   delete [] pbase();
+}
+
+//-----------------------------------------------------------------------------
+
+lengthcalc::lengthcalc( void ) :
+streamwrap( 0, 0xffffffff )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+int lengthcalc::underflow( void )
+{
+   return std::char_traits< char >::eof();
+}
+
+//-----------------------------------------------------------------------------
+
+int lengthcalc::overflow( int ch )
+{
+   pbump( 1 );
+   return ch;
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize lengthcalc::xsgetn( char * s, std::streamsize count )
+{
+   return count;
+}
+
+//-----------------------------------------------------------------------------
+
+std::streamsize lengthcalc::xsputn( const char * s, std::streamsize count )
+{
+   pbump( count );
+   return count;
+}
+
+//-----------------------------------------------------------------------------
+
+buffer_underrun::buffer_underrun( void ) :
+std::runtime_error( "Binary buffer underrun" )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+buffer_overrun::buffer_overrun( void ) :
+std::runtime_error( "Binary buffer overrun" )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+}
+
+//-----------------------------------------------------------------------------
+
+istream & big_endian( istream & in )
+{
+   dynamic_cast< bin::istream & >( in ).filter( &bin::be_filter ) ;
+   return in;
+}
+
+//-----------------------------------------------------------------------------
+
+ostream & big_endian( ostream & out )
+{
+   dynamic_cast< bin::ostream & >( out ).filter( &bin::be_filter ) ;
+   return out;
+}
+
+//-----------------------------------------------------------------------------
+
+istream & little_endian( istream & in )
+{
+   dynamic_cast< bin::istream & >( in ).filter( &bin::le_filter ) ;
+   return in;
+}
+
+//-----------------------------------------------------------------------------
+
+ostream & little_endian( ostream & out )
+{
+   dynamic_cast< bin::ostream & >( out ).filter( &bin::le_filter ) ;
+   return out;
+}
+
+//-----------------------------------------------------------------------------
+
+}
---
+++ src/bin/bin.h
@@ -0,0 +1,200 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 Lucid Systems Pty Ltd - All Rights Reserved
+//
+//-----------------------------------------------------------------------------
+
+#ifndef DS_BIN_H
+#define DS_BIN_H
+
+//-----------------------------------------------------------------------------
+
+#include <ds/ds.h>
+#include <streambuf>
+
+//-----------------------------------------------------------------------------
+
+namespace ds
+{
+
+//-----------------------------------------------------------------------------
+
+namespace bin
+{
+
+//-----------------------------------------------------------------------------
+
+struct filter_t;
+
+//-----------------------------------------------------------------------------
+
+class istream : public ds::istream
+{
+   filter_t * m_filter;
+
+protected: 
+
+   std::streambuf * m_sb;
+
+public:
+
+   istream( std::streambuf * sb = nullptr );
+
+   virtual ds::istream & operator >> ( int8_t & ) override;
+   virtual ds::istream & operator >> ( int16_t & ) override;
+   virtual ds::istream & operator >> ( int32_t & ) override;
+   virtual ds::istream & operator >> ( int64_t & ) override;
+
+   virtual ds::istream & operator >> ( uint8_t & ) override;
+   virtual ds::istream & operator >> ( uint16_t & ) override;
+   virtual ds::istream & operator >> ( uint32_t & ) override;
+   virtual ds::istream & operator >> ( uint64_t & ) override;
+
+   virtual ds::istream & operator >> ( double & ) override;
+
+   void filter( filter_t * filter );
+   void rdbuf( std::streambuf * sb );
+   std::streambuf * rdbuf( void ) const;
+};
+
+//-----------------------------------------------------------------------------
+
+class ostream : public ds::ostream
+{
+   filter_t * m_filter;
+
+protected: 
+
+   std::streambuf * m_sb;
+
+public:
+
+   ostream( std::streambuf * sb = nullptr );
+
+   virtual ds::ostream & operator << ( int8_t ) override;
+   virtual ds::ostream & operator << ( int16_t ) override;
+   virtual ds::ostream & operator << ( int32_t ) override;
+   virtual ds::ostream & operator << ( int64_t ) override;
+
+   virtual ds::ostream & operator << ( uint8_t ) override;
+   virtual ds::ostream & operator << ( uint16_t ) override;
+   virtual ds::ostream & operator << ( uint32_t ) override;
+   virtual ds::ostream & operator << ( uint64_t ) override;
+
+   virtual ds::ostream & operator << ( double ) override;
+
+   void filter( filter_t * filter );
+   void rdbuf( std::streambuf * sb );
+   std::streambuf * rdbuf( void ) const;
+};
+
+//-----------------------------------------------------------------------------
+
+class streamwrap : public std::streambuf
+{
+protected:
+
+   virtual std::streambuf * setbuf( char * s, std::streamsize n ) override;
+
+   virtual int underflow( void ) override;
+   virtual int overflow( int ch ) override;
+
+   virtual std::streamsize xsgetn( char * s, std::streamsize count ) override;
+   virtual std::streamsize xsputn( const char * s, std::streamsize count ) override;
+
+public:
+
+   streamwrap( void * buffer, std::streamsize length );
+
+   void * buffer( void ) const;
+   std::streamsize size( void ) const;
+
+   std::streamsize plength( void ) const;
+   std::streamsize premaining( void ) const;
+   void          * ppos( void ) const;
+
+   std::streamsize glength( void ) const;
+   std::streamsize gremaining( void ) const;
+   void          * gpos( void ) const;
+};
+
+//-----------------------------------------------------------------------------
+
+class streambuf : public streamwrap
+{
+public:
+
+   streambuf( std::streamsize length );
+   virtual ~streambuf( void );
+};
+
+//-----------------------------------------------------------------------------
+
+class lengthcalc : public streamwrap
+{
+protected:
+
+   virtual int underflow( void ) override;
+   virtual int overflow( int ch ) override;
+
+   virtual std::streamsize xsgetn( char * s, std::streamsize count ) override;
+   virtual std::streamsize xsputn( const char * s, std::streamsize count ) override;
+
+public:
+
+   lengthcalc( void );
+};
+
+//-----------------------------------------------------------------------------
+
+template< typename S, typename T > std::streamsize length( const T & t )
+{
+   lengthcalc lc;
+   S out( &lc );
+   out << t;
+   return lc.plength();
+}
+
+//-----------------------------------------------------------------------------
+
+template< typename T > std::streamsize length( const T & t )
+{
+   return length< ostream, T >( t );
+}
+
+//-----------------------------------------------------------------------------
+
+class buffer_underrun : public std::runtime_error
+{
+public:
+
+   buffer_underrun( void );
+};
+
+//-----------------------------------------------------------------------------
+
+class buffer_overrun : public std::runtime_error
+{
+public:
+
+   buffer_overrun( void );
+};
+
+//-----------------------------------------------------------------------------
+
+}
+
+//-----------------------------------------------------------------------------
+
+istream & big_endian( istream & in );
+ostream & big_endian( ostream & out );
+istream & little_endian( istream & in );
+ostream & little_endian( ostream & out );
+
+//-----------------------------------------------------------------------------
+
+}
+
+//-----------------------------------------------------------------------------
+
+#endif
