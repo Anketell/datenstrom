5b98f024578e6cfadb50f17d271f7c6b2798f9a4
Matt Henderson <matt.henderson@lucidsw.com.au>
2022-02-09 10:39:32 +1100

    Separate sqlite context tests

--- src/sqlite/test/test_sqlite_db.cpp
+++ src/sqlite/test/test_sqlite_db.cpp
@@ -4,7 +4,7 @@
 //
 //-----------------------------------------------------------------------------
 
-#include <db/test/test_context.h>
+#include <gtest/gtest.h>
 #include <test_utils/gtest.h>
 #include <db/connection.h>
 #include <sqlite/test/sqlite_test_data.h>
@@ -436,12 +436,5 @@ NAMESPACE_TEST( sqlite, result, should_fail_query_wrong_column_type )
 
    EXPECT_NO_THROW( test_db.drop( test_db_name ) );
 }
-
-//-----------------------------------------------------------------------------
-
-INSTANTIATE_NAMESPACE_TEST_SUITE_P( sqlite,
-                                    SQL_Module,
-                                    Context,
-                                    testing::Values( test_con_str ) );
 
 //-----------------------------------------------------------------------------
