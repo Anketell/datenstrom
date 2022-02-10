2502bfcccbf19d1e3af4ef52b2db4110afa2ae96
Matt Henderson <mhenderson@prydaanz.com>
2022-02-10 15:51:50 +1100

    Fix nuget package

--- scripts/make_image.bat
+++ scripts/make_image.bat
@@ -12,6 +12,7 @@ mkdir build\image\include\bin
 mkdir build\image\lib
 mkdir build\image\bin
 mkdir build\image\module
+mkdir build\image\def
 
 cp -v win32\dsutil\endian.h                                          build\image\include\dsutil
 cp -v win32\dsutil\env.h                                             build\image\include\dsutil
@@ -35,13 +36,16 @@ cp -v src\db\impl.h                                                  build\image
 cp -v src\db\result.h                                                build\image\include\db
 cp -v src\db\sql_map.h                                               build\image\include\db
 cp -v src\db\statement.h                                             build\image\include\db
+cp -v src\db\sql_module_harness.h                                    build\image\include\db
 cp -v src\db\transaction.h                                           build\image\include\db
 cp -v src\db\transactional.h                                         build\image\include\db
 
-cp -v src\bin\bin.h                                                 build\image\include\bin
+cp -v src\bin\bin.h                                                  build\image\include\bin
 
-cp -v build\win32\dsutil\Release\ds_util.dll                           build\image\bin
-cp -v build\win32\dsutil\Release\ds_util.lib                           build\image\lib
+cp -v win32\def\sql_module.def                                       build\image\def
+
+cp -v build\win32\dsutil\Release\ds_util.dll                         build\image\bin
+cp -v build\win32\dsutil\Release\ds_util.lib                         build\image\lib
 
 cp -v build\src\ds\Release\ds.dll                                    build\image\bin
 cp -v build\src\ds\Release\ds.lib                                    build\image\lib
@@ -51,6 +55,7 @@ cp -v build\src\csv\Release\ds_csv.lib                               build\image
 
 cp -v build\src\db\Release\ds_db.dll                                 build\image\bin
 cp -v build\src\db\Release\ds_db.lib                                 build\image\lib
+cp -v build\src\db\Release\sql_module_harness.lib                    build\image\lib
 
 cp -v build\src\bin\Release\ds_bin.dll                               build\image\bin
 cp -v build\src\bin\Release\ds_bin.lib                               build\image\lib
