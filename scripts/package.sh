6bd8b173a677d69e67fa1e365dc33815ce79dcb4
Matt Henderson <matt.henderson@lucidsw.com.au>
2025-03-27 10:12:51 +1100

    Generate spec %files section from image

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,10 +7,11 @@ rm -rf ${root}/release/rpmbuild
 
 ${root}/scripts/make_image.sh
 
-mkdir -p ${root}/release/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-
 export VERSION=$(sed -e "s/^\(.*\)-.*$/\1/" < ${root}/scripts/VERSION)
 export RELEASE=$(sed -e "s/^.*-\(.*\)$/\1/" < ${root}/scripts/VERSION)
+export FILES=$(find ${root}/release/image -type f | sed "s|.*/release/image||")
+
+mkdir -p ${root}/release/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 
 mv ${root}/release/image \
    ${root}/release/rpmbuild/BUILD/datenstrom-devel-${VERSION}
@@ -19,8 +20,8 @@ bash -c "echo \"$(cat ${root}/scripts/datenstrom-devel.spec.tmpl)"\" > \
          ${root}/release/rpmbuild/SPECS/datenstrom-devel.spec
 
 cd release/rpmbuild/BUILD
- tar czf ${root}/release/rpmbuild/SOURCES/datenstrom-devel.tar.gz \
-         datenstrom-devel-${VERSION}
+tar czf ${root}/release/rpmbuild/SOURCES/datenstrom-devel.tar.gz \
+        datenstrom-devel-${VERSION}
 
 cd ${root}
 
